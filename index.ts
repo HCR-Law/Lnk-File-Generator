02d576d536042001eb594841f6faa1b3d001245e
Shivarjun Bhogal <sbhogal@hcrlaw.com>
2025-01-24 09:24:46 +0000

    Started added helper functions

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+/* 
+TODO:
+> Refactor to use Buffer type instead of number[]
+*/
+
 interface Options {
     linkTarget: string;
     name: string;
@@ -6,3 +11,31 @@ interface Options {
     icon_location?: string;
 }
 
+function hexToBuff(hex: string): number[] {
+    const result: number[] = [];
+    let i = 0;
+
+    for (; i < hex.length - 1; i += 2) {
+        result.push(+(`0x` + hex.substring(i, i + 2)));
+    }
+
+    if (hex.length % 2 !== 0) {
+        result.push(+(`0x0` + hex.charAt(i)));
+    }
+
+    return result;
+}
+
+function stringToBuff(str: string): number[] {
+    const result: number[] = [];
+
+    for (let i = 0; i < str.length; ++i) {
+        const charCode: number = str.charCodeAt(i);
+        if (charCode >= 128) {
+            throw new Error(`Non-ASCII char detected: "${str[i]}"`);
+        }
+        result[i] = charCode;
+    }
+
+    return result;
+}
